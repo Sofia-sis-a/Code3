76ddb8fef9372f80fd1fb8fde8efbf6ff472fb55
Sofia-sis-a <sofiakozlova23@gmail.com>
2026-06-08 13:08:38 +0300

    Update CallTracker.h

--- CallTracker.h
+++ CallTracker.h
@@ -2,34 +2,42 @@
 #define CALL_TRACKER_H
 
 #include <iostream>
+#include <fstream>
 
 class CallTracker {
 private:
-    char* subscriberName;       
-    char* phoneNumber;          
-    double maxCallDuration;     
-    double minCallDuration;     
-    double totalDuration;       
-    int totalCalls;             
+    char* subscriberName;
+    char* phoneNumber;
+    double maxCallDuration = 0.0;
+    double minCallDuration = 0.0;
+    double totalDuration = 0.0;
+    int totalCalls = 0;
+
+    static constexpr double FREQUENT_THRESHOLD = 60.0;
 
 public:
     CallTracker(const char* phone, const char* name = nullptr);
     ~CallTracker();
 
+    const char* getName() const;
+    const char* getPhone() const;
     double getMaxDuration() const;
     double getMinDuration() const;
     double getTotalDuration() const;
     int getTotalCalls() const;
+
     double getAverageDuration() const;
     bool isFrequentCommunication() const;
 
     void addCall(double duration);
     void printInfo() const;
 
-    friend std::ostream& operator << (std::ostream& out, const CallTracker& ourObject);
-    friend std::istream& operator >> (std::istream& in, CallTracker& ourObject);
+    // Перегрузка операторов ввода-вывода
+    friend std::ostream& operator<<(std::ostream& out, const CallTracker& obj);
+    friend std::istream& operator>>(std::istream& in, CallTracker& obj);
 };
 
-double& operator += (double& sum, const CallTracker& ourObject);
+// Перегрузка оператора += вне класса
+double& operator+=(double& sum, const CallTracker& obj);
 
 #endif
